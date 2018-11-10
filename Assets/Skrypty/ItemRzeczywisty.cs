bd64b7115a5561867726c75e60d67fde616fdaac
piotrsz109 <piotrsz1098@gmail.com>
2018-11-10 20:03:46 +0100

    Upload HAH

---
+++ Assets/Skrypty/ItemRzeczywisty.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRzeczywisty : MonoBehaviour {
+
+	public Item item;
+
+}
---
+++ Assets/Skrypty/Plecak.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Plecak : MonoBehaviour {
+
+	public int maksymalnaPojemnosc = 5;
+	public List<Item> itemki;
+
+	private void Start () {
+		itemki = new List<Item> ();
+	}
+
+	private void OnTriggerStay (Collider other) {
+		if (other.tag == "Item") {
+			if (Input.GetKeyDown("i")) {
+				itemki.Add (other.GetComponent<ItemRzeczywisty> ().item);
+				Destroy (other.gameObject);
+				Debug.Log (itemki[0].nazwa);
+			}
+		}
+	}
+
+
+
+}
+
+[System.Serializable]
+public class Item {
+
+	public string nazwa;
+	public Texture2D ikona;
+
+	public Item (string n, Texture2D texture2D) {
+		nazwa = n;
+		ikona = texture2D;
+	}
+
+}
