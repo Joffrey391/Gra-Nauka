cec090181ee59fc01b0e485ce8ca6a6eb869e3dc
piotrsz109 <piotrsz1098@gmail.com>
2018-11-14 19:01:26 +0100

    Dodanie zadań do kostek. Dodanie przyciągającego strzelania.

---
+++ Assets/Skrypty/Strzelanie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strzelanie : MonoBehaviour {
+
+	public int ammo, maxAmmo, saveAmmo, maxSaveAmmo;
+
+	RaycastHit hit;
+
+	void Update () {
+
+		if (ammo > 0) {
+			if (Input.GetButtonDown ("Fire1")) {
+				if (Physics.Raycast (transform.position, transform.forward, out hit)) {
+					if(hit.collider.GetComponent<Rigidbody> () != null)
+						hit.collider.GetComponent<Rigidbody> ()?.AddForce (-transform.forward * 15, ForceMode.Impulse);
+				}
+			}
+		}
+
+	}
+}
---
+++ Assets/Skrypty/Zadanie.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Zadanie : MonoBehaviour {
+
+	public Text trescZadaniaText;
+	public GameObject trescZadaniaTlo;
+	public string trescZadania;
+
+	private void Start () {
+		trescZadaniaText = GameObject.Find ("TrescZadania").GetComponent<Text>();
+		trescZadaniaTlo = GameObject.Find ("RawImage");
+	}
+
+	private void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+			trescZadaniaText.text = trescZadania;
+			trescZadaniaTlo.gameObject.SetActive (true);
+		}
+			
+	}
+	
+	private void OnTriggerExit (Collider other) {
+		if (other.tag == "Player")
+			trescZadaniaTlo.gameObject.SetActive (false);
+	}
+
+}
