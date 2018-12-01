6348e03a45c0eb52739a9453d84a209bdbe2c918
Joffrey391 <44951059+Joffrey391@users.noreply.github.com>
2018-12-01 20:21:25 +0100

    Michał
    Dodanie ogniska

---
+++ Assets/Skrypty/Ognisko.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ognisko : MonoBehaviour {
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player") //sprawdzam czy to co weszło w zderzacz to gracz 
+        {
+            if(other.GetComponent<Plecak>().itemki.Any(x =>x.nazwa=="Zapałki")) //jeśli zapałki znajdują się w plecaku gracza
+            {
+                if(Input.GetKeyDown("e")) //jeśli gracz naciście klawisz e
+                {
+                    GetComponent<Renderer>().material.color = Color.red;
+                    //obiekt zmienia kolor na czerwony
+                }
+        
+            }
+        }
+
+    }
+}
